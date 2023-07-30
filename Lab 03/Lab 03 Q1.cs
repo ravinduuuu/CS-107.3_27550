a75f212e835c257a8b6121b2c614fc4b9a480669
ravinduuuu <136630557+ravinduuuu@users.noreply.github.com>
2023-07-30 09:50:20 +0530

    Add files via upload

---
+++ Lab 03/Lab 03 Q1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigitsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int temp = number;
+
+            while (temp != 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            Console.WriteLine("Sum of digits of the number: " + sum);
+        }
+    }
+}
+
+
+
---
+++ Lab 03/Lab 03 Q3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FactorialProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("Factorial of the number: " + factorial);
+        }
+    }
+}
+
+
+
+
+
+
